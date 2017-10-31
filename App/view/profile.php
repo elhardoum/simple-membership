ee8318dd5d92feae0bb2da0259eeecc1033123ed
elhardoum <elhardoum@outlook.com>
2017-10-31 14:36:40 +0000

    dynamic route URLs

--- App/view/profile.php
+++ App/view/profile.php
@@ -3,8 +3,8 @@
     <h1 style="margin-bottom:0">Hello, <?php echo $user->name; ?>!</h1>
 
     <ul>
-        <li><a href="<?php echo url('/profile/edit'); ?>">Edit Profile</a></li>
-        <li><a href="<?php echo url('/profile/edit/password'); ?>">Edit Password</a></li>
+        <li><a href="<?php echo App\Ctrl\ProfileEdit::url(); ?>">Edit Profile</a></li>
+        <li><a href="<?php echo App\Ctrl\ProfileEditPassword::url(); ?>">Edit Password</a></li>
         <li>Not you? <a href="<?php echo App\Ctrl\Logout::url( '?nonce=' . nonce()->create('logout') ); ?>">Logout</a></li>
     </ul>
 </div>
