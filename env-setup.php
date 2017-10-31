6b4fe5c253f2ac9a5714c228dd157d4c263d5840
elhardoum <elhardoum@outlook.com>
2017-10-31 15:07:55 +0000

    .

--- env-setup.php
+++ env-setup.php
@@ -3,30 +3,35 @@
 // prevent direct access
 defined ( 'ROOT_DIR' ) || exit('Direct access not allowed.');
 
-// time constants
-defined ( 'MINUTE_IN_SECONDS' )     || define ( 'MINUTE_IN_SECONDS', 60);
-defined ( 'HOUR_IN_SECONDS' )       || define ( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS);
-defined ( 'DAY_IN_SECONDS' )        || define ( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS);
-defined ( 'WEEK_IN_SECONDS' )       || define ( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS);
-defined ( 'MONTH_IN_SECONDS' )      || define ( 'MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS);
-defined ( 'YEAR_IN_SECONDS' )       || define ( 'YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS);
+// time saver
+function maybe_define($const, $value=null) {
+    return defined ( $const ) || define ( $const, $value );
+}
 
 if ( DEBUG_MODE ) {
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
 }
 
+// time constants
+maybe_define( 'MINUTE_IN_SECONDS', 60 );
+maybe_define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
+maybe_define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
+maybe_define( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS );
+maybe_define( 'MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS );
+maybe_define( 'YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS );
+
 // app root
-define ( 'APP_DIR', ROOT_DIR . '/App' );
+maybe_define( 'APP_DIR', ROOT_DIR . '/App' );
 
 // site URL
-define ( 'SITE_URL', dirname("{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}") );
+maybe_define( 'SITE_URL', dirname("{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}") );
 
-// some important constants
+// some important constants: cookies.
 $url_parts = parse_url(SITE_URL);
-define ( 'DOMAIN', $url_parts['host'] );
-define ( 'PROTOCOL', isset($url_parts['scheme']) ? "{$url_parts['scheme']}://" : 'http://' );
-define ( 'COOKIE_PATH', rtrim(!empty($url_parts['path']) ? $url_parts['path'] : '/', '/') . '/' );
+maybe_define( 'DOMAIN', $url_parts['host'] );
+maybe_define( 'PROTOCOL', isset($url_parts['scheme']) ? "{$url_parts['scheme']}://" : 'http://' );
+maybe_define( 'COOKIE_PATH', rtrim(!empty($url_parts['path']) ? $url_parts['path'] : '/', '/') . '/' );
 
 // setup app autoloader
 spl_autoload_register(function($class) {
@@ -42,4 +47,4 @@ spl_autoload_register(function($class) {
     }
 });
 
-define ( 'AUTH_COOKIE', substr(hash('sha256', SITE_URL . App\Config::SALT), 0, 44) );
+maybe_define( 'AUTH_COOKIE', substr(hash('sha256', SITE_URL . App\Config::SALT), 0, 44) );
